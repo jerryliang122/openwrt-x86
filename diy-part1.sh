9645cecaec50708ca4f96a59c3e2ce19a3dc30b1
Jerry Chen <1261449269@qq.com>
2026-01-13 22:54:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,4 +33,8 @@ echo "luci-app-openclash" >> .git/info/sparse-checkout
 git checkout -b dev
 git pull --depth 1 origin dev
 git branch --set-upstream-to=origin/dev dev
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 
